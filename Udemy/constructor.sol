97fe0bd6a9ae7416932d0ad6a8b3be12d3ee94b5
Irene Fuente Romero <irene.fuente@ultimasoftware.com>
2022-04-26 21:39:53 +0200

    comentarios

--- Udemy/constructor.sol
+++ Udemy/constructor.sol
@@ -3,6 +3,19 @@
 pragma solidity ^0.8.13;
 
 contract FirstContract {
+    //  comentarios de línea
+    /*
+        Comentarios de 
+        bloque
+    */
+    // Comentarios natspec
+    /// @title <Título del contrato>
+    /// @author <Autor del contrato>
+    /// @notice <Explicar lo que hace el código o función>
+    /// @dev <Detalles adicionales sobre el contrato o función>
+    /// @param 
+    /// @return
+
     address owner;
 
     constructor () {
