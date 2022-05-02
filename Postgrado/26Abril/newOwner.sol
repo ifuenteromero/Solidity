0277f7e2dfdc1945faa48de7b843a214d916e2af
Irene Fuente Romero <irene.fuente@ultimasoftware.com>
2022-05-02 15:23:00 +0200

    modifiers

---
+++ Postgrado/26Abril/newOwner.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.13;
+
+contract newOwner {
+    address public owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner {
+        require(
+            msg.sender == owner,
+            "Only owner can call this function."
+        );
+        _;
+    }
+
+    modifier validAddress(address _address) {
+        require(
+            _address != address(0)
+            ,
+            "Not valid address"
+        );
+        _;
+    }
+
+    function changeOwner(address _newOwner) public onlyOwner validAddress(_newOwner) {
+        owner = _newOwner;
+    }
+}
---
+++ Postgrado/26Abril/theory.sol
@@ -0,0 +1,24 @@
+/*
+    CONTENIDOS
+    1 Funciones especiales
+    2. Manejo de errores
+    3. Visibilidad y herencia
+    4. Llamadas entre contratos
+    5. Revisión cambios, convención nomenclaturas
+    6. Revisar actividad anterior
+    7. Actividad 2
+*/
+
+/*
+    • Modificadores de función
+    modificadores de función: Se pueden usar los modificadores
+    para cambiar el comportamiento de las funciones de una manera ágil.
+    Se utilizan principalmente para ejecutar una condición antes de ejecutar
+    una función. Los modificadores son propiedades heredables de los
+    contratos y pueden ser sobrescritos por contratos derivados.
+
+    Modifiers can be used to change the behaviour of functions in a declarative way. 
+    For example, you can use a modifier to automatically check a condition prior to executing the function.
+    Modifiers are inheritable properties of contracts and may be overridden by derived contracts, but only if they are marked virtual.
+    For details, please see Modifier Overriding.
+*/
